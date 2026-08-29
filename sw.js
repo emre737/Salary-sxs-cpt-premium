487592c09b58965aa69c677220ebb24d3903dda2
emre737 <emreoguzksk@hotmail.com>
2026-08-29 23:05:01 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'roster-pay-premium-v1';
+const CORE = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE))); self.skipWaiting();});
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k))))); self.clients.claim();});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(r => {
+      const copy = r.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return r;
+    }).catch(() => cached))
+  );
+});
